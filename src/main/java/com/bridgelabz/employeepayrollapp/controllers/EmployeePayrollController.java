d7ddc12bfaf134491ba0a3f23f26e29ade13cafe
Debojyoti Mukherjee <debojyotimukh@gmail.com>
2020-11-28 18:29:01 +0530

    add Cross origin for live server and json-server

--- src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controllers/EmployeePayrollController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = { "http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000" })
 public class EmployeePayrollController {
 
     @Autowired
